c9a51c4246e66ab2ccd14b2d75fe241803bd8b15
jsan0211 <john.santiago02211992@gmail.com>
2025-05-22 17:14:43 -0500

    Fumbled my save a bit there

--- password_manager.py
+++ password_manager.py
@@ -10,19 +10,44 @@ def get_user_info():
     creds = str(login)
     return creds
 
+def search_login():
+    criteria = input("Enter a site or keyword. :")
+    found = False
+    with open("logins.txt", "r") as file:
+        for line in file:
+            if criteria in line:
+                print(line.strip())
+                found = True
+    if not found:
+        print("No results found.")
 
-with open("logins.txt", "w") as file:
-    file.write(creds)
+def main():
+    while True:
+        print("\nWhat would you like to do?")
+        print("1. Search for a login")
+        print("2. Create a new login")
+        print("3. Generate a password")
+        print("4. Exit password manager")
+        choice = input("Enter your choice [1-4]: ")
+
+        if choice == "1":
+            print("You chose to search for a login.")
+            search_login()
+        elif choice == "2":
+            # Create new login
+            creds = get_user_info()
+            with open("logins.txt", "a") as file:
+                file.write(creds + "\n")
+        elif choice == "3":
+            print("You chose to generate a password.")
+        elif choice == "4":
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice, please try again.")
+
+# Call your main function to start the program
+main()
 
 
-while True:
-    creds = get_user_info()
-    with open("logins.txt", "a") as file:
-        file.write(creds + "\n")
-    add_login = input("Do you want to add another login (yes/no)? :")
-    if add_login.lower() != "yes":
-        break
 
-    def main():
-        while True:
-            print()
